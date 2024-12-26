ade17b2bb2efc28b27b4f753f9ac1e78b1cbbf9a
Alex Jacho <alexjacho77@gmail.com>
2024-12-26 16:37:59 +0200

    Organizating by domains

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './Domains/info/pages/home/home.component';
+
 
 export const routes: Routes = [
     {
