72c291308793f53dac20bdbdf69e4f765623dd37
Alex Jacho <alexjacho77@gmail.com>
2025-01-17 16:56:27 +0200

    Page named -despre- edited with navbar, header and footer

--- src/app/Domains/info/pages/despre-proiect/despre-proiect.component.ts
+++ src/app/Domains/info/pages/despre-proiect/despre-proiect.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-despre-proiect',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent, HeaderComponent],
   templateUrl: './despre-proiect.component.html'
 })
 export class DespreProiectComponent {
