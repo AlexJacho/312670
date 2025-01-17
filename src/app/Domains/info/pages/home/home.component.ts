50edb8ffb0caebc6b7d6db71a2ff1a759e16624b
Alex Jacho <alexjacho77@gmail.com>
2025-01-17 16:38:30 +0200

    App organized. Footer done

--- src/app/Domains/info/pages/home/home.component.ts
+++ src/app/Domains/info/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, CommonModule, HeaderComponent],
+  imports: [NavbarComponent, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
