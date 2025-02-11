a24726065488482c0f69b9d0bb440bbc899faa3b
Alex Jacho <alexjacho77@gmail.com>
2025-02-11 18:27:01 +0200

    routerLink – Linking pages Objective: Enable seamless navigation between the acasa, despre, noutati using Angular’s routerLink for Single[Page-Application (SPA) functionality.

--- src/app/Domains/shared/components/navbar/navbar.component.ts
+++ src/app/Domains/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {OverlayModule} from '@angular/cdk/overlay';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [OverlayModule],
+  imports: [OverlayModule, RouterLinkWithHref,],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
